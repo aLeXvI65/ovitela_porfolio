a0a37229af09d4f32980226ac8b3816d4ca77bb5
aLeXvI65 <vitelaram64@gmail.com>
2024-07-04 17:12:25 -0600

    node env 1

--- src/pages/projects/superHackAttack.jsx
+++ src/pages/projects/superHackAttack.jsx
@@ -11,7 +11,7 @@ const SuperHackAttack = () => {
             <GameProject 
             gameTitle={'super_hack_attack'} 
             description={'super_hack_attack_desc'} 
-            video={ ((process.env.NODE_ENV === 'production') ? '' : '/src') + '/assets/videos/SPH_1_L_1.mp4'}
+            video={ ((process.env.NODE_ENV === 'production') ? '' : '/src') + '/assets/videos/Hack_Demo.mp4'}
             videoType={'video'} />
         </div>
     );
